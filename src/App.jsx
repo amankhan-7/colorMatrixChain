ae2da921cba4fd4c32be1c33823354b8a7826173
amankhan-7 <139583696+amankhan-7@users.noreply.github.com>
2025-04-21 15:04:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import "./App.css"; 
+
+function App() {
+  const [clicks, setClicks] = useState([]);
+  const [animateOrange, setAnimateOrange] = useState(false);
+  const [orange, setOrange] = useState([]);
+
+  useEffect(() => {
+    if (animateOrange && orange.length < 9) {
+      const timer = setTimeout(() => {
+        setOrange((prev) => [...prev, clicks[prev.length]]);
+      }, 300);
+      return () => clearTimeout(timer);
+    }
+  }, [orange, animateOrange, clicks]);
+
+  const handleClick = (i) => {
+    if (clicks.includes(i) || animateOrange) return;
+
+    const updated = [...clicks, i];
+    setClicks(updated);
+
+    if (updated.length === 9) {
+      setAnimateOrange(true);
+    }
+  };
+
+  const getColor = (i) => {
+    if (!clicks.includes(i)) return "default";
+    if (animateOrange) {
+      return orange.includes(i) ? "orange" : "green";
+    }
+    return "green";
+  };
+  
+  return (
+  <div>
+    <h1>ColorMatrix</h1>
+      <div className="grid">  
+      {Array.from({ length: 9 }, (_, i) => (
+        <div
+          key={i}
+          onClick={() => handleClick(i)}
+          className={`box ${getColor(i)}`}
+        >
+          {clicks.indexOf(i) + 1 || ""}
+        </div>
+      ))}
+    </div>
+  </div>
+    
+  );
+}
+
+export default App;
